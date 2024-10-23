d7bc77d14b4db0cc6fa53b19acb338549e904ef5
mpss@devhub <swteam.mpss@gmail.com>
2024-10-23 09:30:48 +0630

    fix callback

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -79,7 +79,7 @@ Octoverse
                 </div>
                 <hr>
                 <div class="clearfix">
-                    <a href="/cart" class="cart-btn back" style="float: right;">BACK</a>
+                        <a href="/cart" class="cart-btn back" style="float: right;">BACK</a>
                 </div>
             </div>
         </div>
@@ -118,13 +118,15 @@ Octoverse
                 const paymentTypeContainer = this.closest('.pay-list').parentNode.parentNode;
                 const paymentTypeElement = paymentTypeContainer.querySelector('h2');
                 const paymentType = paymentTypeElement ? paymentTypeElement.innerText.trim() : '';
+                console.log('Selected Payment Code:', paymentCode);
+                console.log('Selected Payment Type:', paymentType);
                 document.getElementById('selectedPaymentCode').value = paymentCode;
                 showModal(paymentType);
             });
         });
         const modal = document.getElementById('myModal');
         const closeModalBtn = document.getElementsByClassName('close')[0];
-        const modalHeader = document.getElementById('modalHeaderCheck');
+        const modalHeader = document.getElementById('modalHeader');
 
         function showModal(paymentType) {
             modalHeader.innerText = `${paymentType} Payment Information`;
@@ -155,54 +157,54 @@ Octoverse
         }
 
         document.querySelector('.paySubmit').addEventListener('click', function(event) {
-            event.preventDefault();
+    event.preventDefault();
 
-            const submitButton = this;
-            submitButton.disabled = true;
+    const submitButton = this;
+    submitButton.disabled = true; 
 
-            $.ajax({
-                type: 'POST',
-                url: '/checkout',
-                headers: {
-                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                },
-                contentType: 'application/json',
-                data: JSON.stringify({
-                    phoneNo: $('#phoneNo').val(),
-                    email: $('#email').val(),
-                    name: $('#name').val(),
-                    selectedPaymentCode: $('#selectedPaymentCode').val(),
-                    _token: $('meta[name="csrf-token"]').attr('content')
-                }),
-                success: function(response) {
-                    console.log(response);
-                    if (response.message) {
-                        toastr.error(response.message);
-                        submitButton.disabled = false;
-                    } else if (response.data) {
-                        if (response.data.type === 'QR') {
-                            const qrImageElement = document.querySelector('.QR-block img');
-                            qrImageElement.src = response.data.data;
-                            document.querySelector('.QR-block').style.display = 'block';
-                        } else if (response.data.type === 'DEEP_LINK') {
-                            window.location.href = response.data.data;
-                        } else if (response.data.type === 'REDIRECT_URL') {
-                            window.location.href = response.data.data;
-                        } else if (response.data.type === 'MESSAGE') {
-                            toastr.success(response.data.data);
-                            setTimeout(function() {
-                                window.location.href = '/';
-                            }, 100);
-                        }
-                    }
-                },
-                error: function(xhr, status, error) {
-                    console.error("Error occurred: ", error);
-                    toastr.error("An error occurred. Please try again.");
-                    submitButton.disabled = false;
+    $.ajax({
+        type: 'POST',
+        url: '/checkout',
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        contentType: 'application/json',
+        data: JSON.stringify({
+            phoneNo: $('#phoneNo').val(),
+            email: $('#email').val(),
+            name: $('#name').val(),
+            selectedPaymentCode: $('#selectedPaymentCode').val(),
+            _token: $('meta[name="csrf-token"]').attr('content')
+        }),
+        success: function(response) {
+            console.log(response);
+            if (response.message) {
+                toastr.error(response.message);
+                submitButton.disabled = false;  
+            } else if (response.data) {
+                if (response.data.type === 'QR') {
+                    const qrImageElement = document.querySelector('.QR-block img');
+                    qrImageElement.src = response.data.data;
+                    document.querySelector('.QR-block').style.display = 'block';
+                } else if (response.data.type === 'DEEP_LINK') {
+                    window.location.href = response.data.data;
+                } else if (response.data.type === 'REDIRECT_URL') {
+                    window.location.href = response.data.data; 
+                } else if (response.data.type === 'MESSAGE') {
+                    toastr.success(response.data.data);
+                    setTimeout(function() {
+                        window.location.href = '/';  
+                    }, 100);
                 }
-            });
-        });
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error("Error occurred: ", error);
+            toastr.error("An error occurred. Please try again.");
+            submitButton.disabled = false; 
+        }
+    });
+});
 
     });
 </script>
