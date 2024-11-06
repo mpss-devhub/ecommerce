b64bfe2c57b6533f28decbf723bf46fb122450ae
MoeChanMyaeHtun <moechan599@gmail.com>
2024-11-06 15:36:13 +0630

    fixed css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.frontend.master')
 @section('content')
-<div class="home-mv">
+<div class="home-mv"><img src="{{ asset ('img/10-img.png') }}" alt="" class="octoverse">
 	<div class="txt-box">
 		<div class="slide-ani clearfix">
 			<img src="{{ asset ('img/10-img.png') }}" alt="">
@@ -11,7 +11,7 @@
 		</p>
 		<p class="myanmar-text">ယခု Website သည် Octoverse Payment Gateway ၏ Sample Payment Flow အား Testing ပြုလုပ်နိုင်စေရန် ရည်ရွယ်သည့် Demo Website ဖြစ်ပါသည်။ ယခု Website မှ Items များကို ဝယ်ယူပါက အဆိုပါပစ္စည်းအား အမှန်တကယ်ရရှိမည် မဟုတ်သော်လည်း လူကြီးမင်း၏ Bank Account မှ အဆိုပါ ပစ္စည်းတန်ဖိုး၏ ကျသင့်ငွေ ဖြတ်သွားမည်ဖြစ်ပါကြောင်း အသိပေးကြေညာ အပ်ပါသည်။ </p>
 
-		<p class="" style="color: red;">
+		<p class="eng-text" style="color: red;">
 			*** The minimum amount is 1, and the maximum amount is 1000. ***
 		</p>
 		<a href="/products" class="shopNowBtn">Shop Now</a>
