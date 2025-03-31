ec7ce92b5edb105c4123d9667145aa8e06a6dcc0
Phyo-Khant-Kyaw <www.pkkcobra01@gmail.com>
2025-03-31 15:47:31 +0700

    updated css

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -8,16 +8,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Octoverse</title>
     <link rel="icon" type="image/png" href="{{ asset('img/10-img.png') }}" class="w-6">
-    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/cart.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/product.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/checkout.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/history.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/cart.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/product.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/checkout.css') }}?v={{ time() }}">
+    <link rel="stylesheet" href="{{ asset('css/history.css') }}?v={{ time() }}">
     <link rel="stylesheet" href="{{ asset('fontawesome/all.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/toastr.min.css') }}?v={{ time() }}">
 </head>
 
 <body>
@@ -42,28 +42,28 @@
                         </a>
                     </li>
                     @auth
-                        <li><a href="{{ url('/logout') }}" class="logout">Logout</a></li>
+                    <li><a href="{{ url('/logout') }}" class="logout">Logout</a></li>
                     @else
-                        <li class="dropdown pc">
-                            <button class="dropbtn pc">
-                                <i class="fas fa-sign-in-alt signin-icon"></i>
-                            </button>
-                            <div class="dropdown-content pc">
-                                <a href="{{ route('login') }}">Login</a>
-                                <a href="{{ route('register') }}"
-                                    class="{{ Request::is('/register') ? 'active' : '' }}">Register</a>
-                            </div>
-                        </li>
-                        <li class="  sp">
-                            <ul class="list">
-                                <li><a href="{{ route('register') }}"
-                                        class="{{ Request::is('/register') ? 'active' : '' }}  sp">Register</a></li>
-                                <li class="sp"><a href="{{ route('login') }} ">Login</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
+                    <li class="dropdown pc">
+                        <button class="dropbtn pc">
+                            <i class="fas fa-sign-in-alt signin-icon"></i>
+                        </button>
+                        <div class="dropdown-content pc">
+                            <a href="{{ route('login') }}">Login</a>
+                            <a href="{{ route('register') }}"
+                                class="{{ Request::is('/register') ? 'active' : '' }}">Register</a>
+                        </div>
                     </li>
+                    <li class="  sp">
+                        <ul class="list">
+                            <li><a href="{{ route('register') }}"
+                                    class="{{ Request::is('/register') ? 'active' : '' }}  sp">Register</a></li>
+                            <li class="sp"><a href="{{ route('login') }} ">Login</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                </li>
                 @endauth
                 </ul>
             </nav>
@@ -103,4 +103,5 @@
     <!-- @stack('js') -->
 
 </body>
+
 </html>
